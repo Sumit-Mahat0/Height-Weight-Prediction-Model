aa34388a4b71143873e6ac52b4d00f7aad3d9b76
Sumit Mahato <139648871+Sumit-Mahat0@users.noreply.github.com>
2024-07-02 23:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,11 +44,14 @@ height_input = st.number_input("Enter the height in feet:", value=default_height
 
 # Predict button
 if st.button('Predict'):
-    # Reshape the input height to match the shape expected by the model (2D array)
-    height_input_2d = np.array(height_input).reshape(1, -1)
+    try:
+        # Reshape the input height to match the shape expected by the model (2D array)
+        height_input_2d = np.array(height_input).reshape(1, -1)
 
-    # Use the loaded model to make predictions
-    predicted_weight = loaded_model.predict(height_input_2d)
+        # Use the loaded model to make predictions
+        predicted_weight = loaded_model.predict(height_input_2d)
 
-    # Print the predicted weight
-    st.markdown(f'<p class="prediction">Predicted weight: {predicted_weight[0, 0]} kg</p>', unsafe_allow_html=True)
+        # Print the predicted weight
+        st.markdown(f'<p class="prediction">Predicted weight: {predicted_weight[0][0]:.2f} kg</p>', unsafe_allow_html=True)
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
